8db1f3c5ac0fe00993e44596b09f7933918a1b1c
Tejas Shettigar <114768357+TEZZ444@users.noreply.github.com>
2024-02-26 22:35:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const host = '0.0.0.0';
 
 app.use(express.static('public'));
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+
+app.listen(port, host, () => {
+  console.log(`Server is running at http://${host}:${port}`);
 });
